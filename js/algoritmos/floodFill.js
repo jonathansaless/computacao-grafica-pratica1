3735ccb576a4e5e68911a3f9bf4532f579a3cd2b
Jonathan Sales <51421355+jonathansaless@users.noreply.github.com>
2023-05-20 23:55:36 -0300

    preenchimento recursivo

---
+++ js/algoritmos/floodFill.js
@@ -0,0 +1,31 @@
+import { drawPixel } from "./drawPixel.js";
+import * as colors from "../colors.js";
+
+// preechimento recursivo OK
+export function floodFill(x, y){
+    // cor da borda
+    const edgeColor = colors.RED
+    // cor a ser pintada
+    const color = colors.GREEN
+
+    // pega o quadrado atual
+    const cellId = `${x}_${y}`;
+    const cellElement = document.getElementById(cellId);
+    
+    // verifica se a celular recebida não é nula, pois se o cellId estiver fora dos IDs, não retornará nenhuma div
+    if(cellElement === null){
+        return
+    }
+
+    // se o quadrado atual não estiver pintado nem de vermelho nem de verde, entra no IF
+    if(!cellElement.classList.contains('painted')) {
+        // pinta de verde
+        drawPixel(x, y, color);
+        // preenche os demais recursivamente
+        floodFill(x+1, y);
+        floodFill(x, y+1);
+        floodFill(x-1, y);
+        floodFill(x, y-1);
+    }
+    return
+}
