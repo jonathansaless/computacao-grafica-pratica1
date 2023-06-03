f2449034fd1b1f2491a75901d0c33f6f93564211
jonathansaless <jonathan.sales@trt8.jus.br>
2023-06-03 14:43:27 -0300

    update

--- scripts/algorithms/drawings/drawCurve.js
+++ scripts/algorithms/drawings/drawCurve.js
@@ -1,19 +1,23 @@
 import { drawPixel } from "../drawPixel.js";
 
+// Função para desenhar uma curva suave de Bezier
 export function drawCurve(startX, startY, endX, endY, controlPoints, color) {
+    // Cria um array de pontos contendo o ponto inicial, os pontos de controle e o ponto final
     const points = [
-        { x: startX, y: startY },
-        ...controlPoints,
-        { x: endX, y: endY }
+        { x: startX, y: startY },   // Ponto inicial
+        ...controlPoints,           // Pontos de controle (array espalhado)
+        { x: endX, y: endY }        // Ponto final
     ];
     
-    const segments = 100;
-    const tDelta = 1 / segments;
+    const segments = 100;           // Número de segmentos da curva
+    const tDelta = 1 / segments;    // Intervalo entre cada segmento
 
+    // Itera sobre os segmentos da curva
     for (let t = 0; t <= 1; t += tDelta) {
-        let p = points;
+        let p = points;     // Define o array de pontos atual
         let pTemp;
 
+        // Executa a interpolação dos pontos até que reste apenas um ponto
         while (p.length > 1) {
             pTemp = [];
             for (let i = 0; i < p.length - 1; i++) {
@@ -25,13 +29,14 @@ export function drawCurve(startX, startY, endX, endY, controlPoints, color) {
             p = pTemp;
         }
 
-        const pFinal = p[0];
-        drawPixel(Math.round(pFinal.x), Math.round(pFinal.y), color);
+        const pFinal = p[0];    // Obtém o ponto final da curva no intervalo atual
+        drawPixel(Math.round(pFinal.x), Math.round(pFinal.y), color);   // Desenha o ponto final da curva na tela
     }
 }
 
+// Função auxiliar para interpolar coordenadas entre dois pontos
 function interpolate(p0, p1, t) {
-    const x = p0.x + (p1.x - p0.x) * t;
-    const y = p0.y + (p1.y - p0.y) * t;
-    return { x, y };
+    const x = p0.x + (p1.x - p0.x) * t; // Interpola a coordenada x
+    const y = p0.y + (p1.y - p0.y) * t; // Interpola a coordenada y
+    return { x, y };    // Retorna o ponto interpolado
 }
